1c9c86b6e207f9e5aa09c43b1e317cca1f3c5635
davidstrasak <davidstrasak@seznam.cz>
2025-11-03 22:35:22 +0100

    we are multiplexing correctly and it has a counter that counts seconds and displays them on the displays

--- Source/main.c
+++ Source/main.c
@@ -7,8 +7,8 @@
 //;*
 //;**************************************************************************************************
 //;*
-//;* JMÉNO PROJEKTU	: Demo_01
-//;* AUTOR			: Radek Øípa
+//;* JMï¿½NO PROJEKTU	: Demo_01
+//;* AUTOR			: Radek ï¿½ï¿½pa
 //;* DATUM			: 11/2011
 //;* POPIS			: Program pro ovladani LED na vyvodu PC8 pomoci USER tlacitka na PA0.
 //;*					  - konfigurace hodin na frekvenci 24MHz (HSE + PLL) 
@@ -51,6 +51,7 @@ void GPIO_Configuration(void);
 void Delay(vu32 nCount);
 void setNumber(int leftInput, int rightInput);
 void removeFirst(void);
+void displayMultiplex(int leftNum1, int leftNum2, int rightNum1, int rightNum2);
 
 /*Metody*/
 
@@ -61,55 +62,55 @@ void removeFirst(void);
  *Jinak se inicializace provadi v souboru system_stm32f10x.h
 
 */
-void SystemInit(void){
-//Prazdna inicializacni metoda	
+void SystemInit(void) {
+	//Prazdna inicializacni metoda	
 }
 
 /*Main funkce*/
-int main(void){
+int main(void) {
 	int counter = 0;
 	volatile uint32_t message = 0;
 	volatile uint32_t nextBit;
-	
+	int multiplexCounter = 0;
+
 	RCC_Configuration(); //inicializace hodin
 	GPIO_Configuration(); //inicializace GPIO
-	
-	GPIOA->BSRR|=(1 << (10));
-	GPIOD->BSRR|=(1 << (2));
-	
-	while(counter < 15){
+
+	GPIOA->BSRR |= (1 << (10));
+	GPIOD->BSRR |= (1 << (2));
+
+	while (counter < 15) {
 		// timhle bych dostal full sviticich ledek
 		//GPIOC->BSRR|=(1<<(10 + 16));
-		
+
 		// timto bych mel dostat full nesviticich ledek
-		GPIOC->BSRR|=(1<<(6));
-		GPIOC->BSRR|=(1<<(7));
-		GPIOB->BSRR|=(1<<(5));
-		GPIOB->BSRR|=(1<<(6));
-		
-		GPIOC->BSRR|=(1 << (12));
-		GPIOC->BSRR|=(1 << (12 + 16));
+		GPIOC->BSRR |= (1 << (6));
+		GPIOC->BSRR |= (1 << (7));
+		GPIOB->BSRR |= (1 << (5));
+		GPIOB->BSRR |= (1 << (6));
+
+		GPIOC->BSRR |= (1 << (12));
+		GPIOC->BSRR |= (1 << (12 + 16));
 
 		counter++;
 	}
-	
+
 	counter = 0;
-	
-	
+
+
 	/*Nekonecna smycka*/
-	while(1){
-		GPIOA->BSRR|=(1 << (10+16));
-		GPIOD->BSRR|=(1 << (2));
-		setNumber(1,5);
-		Delay(10);
-		removeFirst();
-		//Delay(1000);
-		GPIOA->BSRR|=(1 << (10));
-		GPIOD->BSRR|=(1 << (2+16));
-		setNumber(8,8);
-		Delay(10);
-		removeFirst();
-		//Delay(1000);
+	while (1) {
+		int modCount = (counter / 10) % 10;
+		int modModCount = (counter / 100) % 10;
+		int modModModCount = (counter / 1000) % 10;
+		displayMultiplex(modModModCount, modModCount, modCount, counter % 10);
+
+		// Multiplex bezi rychle, ale counter se inkrementuje az po ~100 cyklech (= ~1 sekunda)
+		multiplexCounter++;
+		if (multiplexCounter >= 100) {
+			multiplexCounter = 0;
+			counter++;
+		}
 	}
 }
 
@@ -120,189 +121,210 @@ void setNumber(int leftInput, int rightInput)
 	int counter = 0;
 	int nextBitLeft;
 	int nextBitRight;
-	
-		switch(leftInput){
-		case 0:
-			messageLeft = Number0;
-			break;
-		case 1:
-			messageLeft = Number1;
-			break;
-		case 2:
-			messageLeft = Number2;
-			break;
-		case 3:
-			messageLeft = Number3;
-			break;
-		case 4:
-			messageLeft = Number4;
-			break;
-		case 5:
-			messageLeft = Number5;
-			break;
-		case 6:
-			messageLeft = Number6;
-			break;
-		case 7:
-			messageLeft = Number7;
-			break;
-		case 8:
-			messageLeft = Number8;
-			break;
-		case 9:
-			messageLeft = Number9;
-			break;
-		default:
-			messageLeft = Number_OFF;
-			break;
+
+	switch (leftInput) {
+	case 0:
+		messageLeft = Number0;
+		break;
+	case 1:
+		messageLeft = Number1;
+		break;
+	case 2:
+		messageLeft = Number2;
+		break;
+	case 3:
+		messageLeft = Number3;
+		break;
+	case 4:
+		messageLeft = Number4;
+		break;
+	case 5:
+		messageLeft = Number5;
+		break;
+	case 6:
+		messageLeft = Number6;
+		break;
+	case 7:
+		messageLeft = Number7;
+		break;
+	case 8:
+		messageLeft = Number8;
+		break;
+	case 9:
+		messageLeft = Number9;
+		break;
+	default:
+		messageLeft = Number_OFF;
+		break;
 	}
-	
-	switch(rightInput){
-		case 0:
-			messageRight = Number0;
-			break;
-		case 1:
-			messageRight = Number1;
-			break;
-		case 2:
-			messageRight = Number2;
-			break;
-		case 3:
-			messageRight = Number3;
-			break;
-		case 4:
-			messageRight = Number4;
-			break;
-		case 5:
-			messageRight = Number5;
-			break;
-		case 6:
-			messageRight = Number6;
-			break;
-		case 7:
-			messageRight = Number7;
-			break;
-		case 8:
-			messageRight = Number8;
-			break;
-		case 9:
-			messageRight = Number9;
-			break;
-		default:
-			messageRight = Number_OFF;
-			break;
+
+	switch (rightInput) {
+	case 0:
+		messageRight = Number0;
+		break;
+	case 1:
+		messageRight = Number1;
+		break;
+	case 2:
+		messageRight = Number2;
+		break;
+	case 3:
+		messageRight = Number3;
+		break;
+	case 4:
+		messageRight = Number4;
+		break;
+	case 5:
+		messageRight = Number5;
+		break;
+	case 6:
+		messageRight = Number6;
+		break;
+	case 7:
+		messageRight = Number7;
+		break;
+	case 8:
+		messageRight = Number8;
+		break;
+	case 9:
+		messageRight = Number9;
+		break;
+	default:
+		messageRight = Number_OFF;
+		break;
 	}
-	
+
 	messageLeft = messageLeft << 1;
 	messageRight = messageRight << 1;
-	
-	GPIOC->BSRR|=(1 << (6));
-	GPIOB->BSRR|=(1 << (5));
 
-	while(counter != -1)
+	GPIOC->BSRR |= (1 << (6));
+	GPIOB->BSRR |= (1 << (5));
+
+	while (counter != -1)
 	{
-		
+
 		messageLeft = messageLeft >> 1;
 		messageRight = messageRight >> 1;
-		
+
 		nextBitLeft = messageLeft & 1;
 		nextBitRight = messageRight & 1;
-		
-		if(counter < 8){
-			if(nextBitLeft == 1)
+
+		if (counter < 8) {
+			if (nextBitLeft == 1)
 			{
-				GPIOC->BSRR|=(1<<(7 + 16)); // obracena logika
-			} 
-			else
-			{
-				GPIOC->BSRR|=(1<<(7));
-			}
-			if(nextBitRight == 1)
-			{
-				GPIOB->BSRR|=(1<<(6 + 16)); // obracena logika
+				GPIOC->BSRR |= (1 << (7 + 16)); // obracena logika
 			}
 			else
 			{
-				GPIOB->BSRR|=(1<<(6)); 
+				GPIOC->BSRR |= (1 << (7));
+			}
+			if (nextBitRight == 1)
+			{
+				GPIOB->BSRR |= (1 << (6 + 16)); // obracena logika
+			}
+			else
+			{
+				GPIOB->BSRR |= (1 << (6));
 			}
 		}
-		else{
+		else {
 			counter = -1;
 			continue;
 		}
-		
-		GPIOC->BSRR|=(1 << (12));
-		GPIOC->BSRR|=(1 << (12 + 16));
+
+		GPIOC->BSRR |= (1 << (12));
+		GPIOC->BSRR |= (1 << (12 + 16));
 		counter++;
 	}
-	
-	GPIOC->BSRR|=(1 << (6 + 16));
-	GPIOB->BSRR|=(1 << (5 + 16));
+
+	GPIOC->BSRR |= (1 << (6 + 16));
+	GPIOB->BSRR |= (1 << (5 + 16));
 }
 
 void removeFirst(void) {
-	
+
 	int counter = 0;
-	
-	GPIOC->BSRR|=(1 << (6));
-	GPIOB->BSRR|=(1 << (5));
 
-	while(counter != -1)
-	{		
+	GPIOC->BSRR |= (1 << (6));
+	GPIOB->BSRR |= (1 << (5));
 
-			GPIOC->BSRR|=(1<<(7)); 
-			GPIOB->BSRR|=(1<<(6)); 
+	while (counter != -1)
+	{
 
-		if(counter == 8){
+		GPIOC->BSRR |= (1 << (7));
+		GPIOB->BSRR |= (1 << (6));
+
+		if (counter == 8) {
 			counter = -1;
 			continue;
 		}
-		
-		GPIOC->BSRR|=(1 << (12));
-		GPIOC->BSRR|=(1 << (12 + 16));
+
+		GPIOC->BSRR |= (1 << (12));
+		GPIOC->BSRR |= (1 << (12 + 16));
 		counter++;
 	}
-	
-	GPIOC->BSRR|=(1 << (6 + 16));
-	GPIOB->BSRR|=(1 << (5 + 16));
 
+	GPIOC->BSRR |= (1 << (6 + 16));
+	GPIOB->BSRR |= (1 << (5 + 16));
+
+}
+
+/*Funkce pro multiplexovani 4 cisel na dvou blocich 7-segmentovek
+ *leftNum1, leftNum2 = cisla pro levy blok (PA10)
+ *rightNum1, rightNum2 = cisla pro pravy blok (PD2)
+ *Provede jeden kompletni cyklus multiplexovani
+*/
+void displayMultiplex(int leftNum1, int leftNum2, int rightNum1, int rightNum2) {
+	// Zobrazeni praveho bloku (PD2 aktivni)
+	GPIOA->BSRR |= (1 << 10);         // PA10 high - vypni levy blok
+	GPIOD->BSRR |= (1 << (2 + 16));   // PD2 low - zapni pravy blok
+	setNumber(leftNum1, rightNum1);
+	Delay(10);
+	removeFirst();
+
+	// Zobrazeni leveho bloku (PA10 aktivni)
+	GPIOA->BSRR |= (1 << (10 + 16));  // PA10 low - zapni levy blok
+	GPIOD->BSRR |= (1 << 2);          // PD2 high - vypni pravy blok
+	setNumber(leftNum2, rightNum2);
+	Delay(10);
+	removeFirst();
 }
 
 /*Inicializace RCC*/
-void RCC_Configuration(void){
-	RCC->CR|=0x10000; //HSE on
-	while(!(RCC->CR&0x20000)){}
-	  //flash access setup
-  	FLASH->ACR &= 0x00000038;   //mask register
-  	FLASH->ACR |= 0x00000002;   //flash 2 wait state
+void RCC_Configuration(void) {
+	RCC->CR |= 0x10000; //HSE on
+	while (!(RCC->CR & 0x20000)) {}
+	//flash access setup
+	FLASH->ACR &= 0x00000038;   //mask register
+	FLASH->ACR |= 0x00000002;   //flash 2 wait state
 
- 	FLASH->ACR &= 0xFFFFFFEF;   //mask register
-    FLASH->ACR |= 0x00000010;   //enable Prefetch Buffer
+	FLASH->ACR &= 0xFFFFFFEF;   //mask register
+	FLASH->ACR |= 0x00000010;   //enable Prefetch Buffer
 
-	RCC->CFGR&=0xFFC3FFFF;//maskovani PLLMUL
-	RCC->CFGR|=0x1<<18;//Nastveni PLLMUL 3x
-	RCC->CFGR|=0x0<<17;//nastaveni PREDIV1 1x
-	RCC->CFGR|=0x10000;//PLL bude clocovan z PREDIV1
-	RCC->CFGR&=0xFFFFFF0F;//HPRE=1x
-	RCC->CFGR&=0xFFFFF8FF;//PPRE2=1x
-	RCC->CFGR&=0xFFFFC7FF;//PPRE2=1x
+	RCC->CFGR &= 0xFFC3FFFF;//maskovani PLLMUL
+	RCC->CFGR |= 0x1 << 18;//Nastveni PLLMUL 3x
+	RCC->CFGR |= 0x0 << 17;//nastaveni PREDIV1 1x
+	RCC->CFGR |= 0x10000;//PLL bude clocovan z PREDIV1
+	RCC->CFGR &= 0xFFFFFF0F;//HPRE=1x
+	RCC->CFGR &= 0xFFFFF8FF;//PPRE2=1x
+	RCC->CFGR &= 0xFFFFC7FF;//PPRE2=1x
 
-	RCC->CR|=0x01000000;//PLL on
-	while(!(RCC->CR&0x02000000)){}//PLL stable??
+	RCC->CR |= 0x01000000;//PLL on
+	while (!(RCC->CR & 0x02000000)) {}//PLL stable??
 
-	RCC->CFGR&=0xFFFFFFFC;
-	RCC->CFGR|=0x2;//nastaveni PLL jako zdroj hodin pro SYSCLK
+	RCC->CFGR &= 0xFFFFFFFC;
+	RCC->CFGR |= 0x2;//nastaveni PLL jako zdroj hodin pro SYSCLK
 
-  	while(!(RCC->CFGR & 0x00000008))//je SYSCLK nastaveno?
-  	{
-  	}
+	while (!(RCC->CFGR & 0x00000008))//je SYSCLK nastaveno?
+	{
+	}
 
-	RCC->APB2ENR|= (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); // Enable PA (2), PB (3), PC (4) and PD (5).
-		
+	RCC->APB2ENR |= (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); // Enable PA (2), PB (3), PC (4) and PD (5).
+
 }
 /*Inicializace GPIO*/
-void GPIO_Configuration(void){
-	
+void GPIO_Configuration(void) {
+
 	// PA10 - Napajeni pro level displej
 	GPIOA->CRH &= ~(0xF << 8);
 	GPIOA->CRH |= (3 << 8);  // PP output
@@ -330,8 +352,8 @@ void GPIO_Configuration(void){
 /*Delay smycka zpozduje zhruba o nCount milisekund*/
 void Delay(vu32 nCount)
 {
-  for(; nCount != 0; nCount--) {
-			int i = 6000;
-			for(; i != 0; i--);
+	for (; nCount != 0; nCount--) {
+		int i = 6000;
+		for (; i != 0; i--);
 	}
 }
